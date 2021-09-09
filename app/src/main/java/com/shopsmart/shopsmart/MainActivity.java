feb72e4e4d540958141b64b7023be96786a4e8f0
jchan201 <54863433+jchan201@users.noreply.github.com>
2021-09-08 21:23:59 -0400

    Added some sample code for accessing the database.

--- app/src/main/java/com/shopsmart/shopsmart/MainActivity.java
+++ app/src/main/java/com/shopsmart/shopsmart/MainActivity.java
@@ -3,9 +3,21 @@ package com.shopsmart.shopsmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import io.realm.Realm;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoCollection;
+import io.realm.mongodb.mongo.MongoDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private App app;
+    private MongoClient mongoClient;
+    private MongoDatabase mongoDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +25,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Realm.init(this);
+        app = new App(new AppConfiguration.Builder("shopsmart-acsmx").build());
+
+        // (EXAMPLE) Log in:
+        Credentials credentials = Credentials.emailPassword("something", "something");
+        app.loginAsync(credentials/*Credentials.anonymous()*/, new App.Callback<User>() {
+            @Override
+            public void onResult(App.Result<User> result) {
+                if (result.isSuccess()) {
+                    Log.v("User", "Logged In Successfully");
+                    User user = app.currentUser();
+                    mongoClient = user.getMongoClient("mongodb-atlas");
+                    mongoDatabase = mongoClient.getDatabase("ShopSmartDB");
+                    MongoCollection mongoCollection = mongoDatabase.getCollection("someCollection");
+                }
+                else {
+                    Log.v("User", "Failed to Login");
+                }
+            }
+        });
+        // (EXAMPLE) Register:
+        app.getEmailPassword().registerUserAsync("something", "something", it -> {
+            if (it.isSuccess()) {
+                Log.v("User", "Registered with email successfully");
+            }
+            else {
+                Log.v("User", "Registration failed");
+            }
+        });
     }
 }
