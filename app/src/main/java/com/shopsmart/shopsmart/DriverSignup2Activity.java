60851cdce495c6a57c18140930693d37e159f495
Calvin <bintot914@gmail.com>
2021-09-27 19:13:04 -0400

    Created first 2 pages of Driver sign up activity

---
+++ app/src/main/java/com/shopsmart/shopsmart/DriverSignup2Activity.java
@@ -0,0 +1,14 @@
+package com.shopsmart.shopsmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverSignup2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_signup2);
+    }
+}
