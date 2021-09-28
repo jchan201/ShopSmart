3025c7ea5955d164a03a23120f5eed2ab66ba7db
Calvin <bintot914@gmail.com>
2021-09-28 15:01:28 -0400

    UI for Driver Profile completed

--- app/src/main/java/com/shopsmart/shopsmart/DriverProfileActivity.java
+++ app/src/main/java/com/shopsmart/shopsmart/DriverProfileActivity.java
@@ -3,12 +3,20 @@ package com.shopsmart.shopsmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.shopsmart.shopsmart.databinding.ActivityDriverDashboardBinding;
+import com.shopsmart.shopsmart.databinding.ActivityDriverProfileBinding;
 
 public class DriverProfileActivity extends AppCompatActivity {
+    ActivityDriverProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_profile);
+
+        this.binding = ActivityDriverProfileBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
