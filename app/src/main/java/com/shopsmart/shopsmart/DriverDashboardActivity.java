e7a2cdae9815e379e93ec9cf058130ee0915108d
Calvin <bintot914@gmail.com>
2021-09-28 10:33:31 -0400

    Button to user profile completed

--- app/src/main/java/com/shopsmart/shopsmart/DriverDashboardActivity.java
+++ app/src/main/java/com/shopsmart/shopsmart/DriverDashboardActivity.java
@@ -2,13 +2,47 @@ package com.shopsmart.shopsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class DriverDashboardActivity extends AppCompatActivity {
+import com.shopsmart.shopsmart.databinding.ActivityDriverDashboardBinding;
+
+public class DriverDashboardActivity extends AppCompatActivity implements View.OnClickListener {
+    ActivityDriverDashboardBinding binding;
+    AppUser appUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_dashboard);
+
+        this.binding = ActivityDriverDashboardBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Get Intent
+        Intent currIntent = this.getIntent();
+
+        if (currIntent != null) {
+            // Grab objects from intent
+            this.appUser = (AppUser)currIntent.getSerializableExtra("EXTRA_APPUSER_OBJ");
+        }
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        if (view != null) {
+            switch (view.getId()) {
+                case R.id.btn_profile: {
+                    // Go to Driver profile
+                    Intent profileIntent = new Intent(this, DriverProfileActivity.class);
+                    profileIntent.putExtra("EXTRA_APPUSER_OBJ", this.appUser);
+                    startActivity(profileIntent);
+                    break;
+                }
+            }
+        }
+    }
+
+
+} // end of class
