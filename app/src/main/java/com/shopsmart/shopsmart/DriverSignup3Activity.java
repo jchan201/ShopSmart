a273da83d4f4cb4498ad868503d28f1a0c73c817
Calvin <bintot914@gmail.com>
2021-09-28 10:21:08 -0400

    Finished UI and partial functionality for Driver Sign up page 3.

--- app/src/main/java/com/shopsmart/shopsmart/DriverSignup3Activity.java
+++ app/src/main/java/com/shopsmart/shopsmart/DriverSignup3Activity.java
@@ -9,9 +9,8 @@ import android.view.View;
 import com.shopsmart.shopsmart.databinding.ActivityDriverSignup2Binding;
 import com.shopsmart.shopsmart.databinding.ActivityDriverSignup3Binding;
 
-public class DriverSignup3Activity extends AppCompatActivity {
+public class DriverSignup3Activity extends AppCompatActivity implements View.OnClickListener {
     ActivityDriverSignup3Binding binding;
-
     AppUser appUser;
     String userPassword;
 
@@ -31,5 +30,62 @@ public class DriverSignup3Activity extends AppCompatActivity {
             this.userPassword = currIntent.getStringExtra("EXTRA_PASSWORD");
             this.appUser = (AppUser)currIntent.getSerializableExtra("EXTRA_APPUSER_OBJ");
         }
+
+        this.binding.btnCancel.setOnClickListener(this);
+        this.binding.btnNext.setOnClickListener(this);
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        if (view != null) {
+            switch (view.getId()) {
+                case R.id.btn_cancel: {
+                    // Go back to login page
+                    Intent mainIntent = new Intent(this, StartupActivity.class);
+                    startActivity(mainIntent);
+                    break;
+                }
+                case R.id.btn_next: {
+                    // Validate data
+                    if (this.validateData()) {
+                        // Update AppUser Object
+                        BankInformation bankInformation = new BankInformation();
+                        bankInformation.setAccountNumber(this.binding.editAccountNumber.getText().toString());
+                        bankInformation.setInstitutionNumber(this.binding.editInstitutionNumber.getText().toString());
+                        bankInformation.setTransitNumber(this.binding.editTransitNumber.getText().toString());
+                        this.createUser();
+
+                        // Go to Dashboard
+                        Intent dashboardIntent = new Intent(this, DriverDashboardActivity.class);
+                        dashboardIntent.putExtra("EXTRA_APPUSER_OBJ", this.appUser);
+                        startActivity(dashboardIntent);
+                    }
+                    break;
+                }
+            } // end of switch
+        }
+    }
+
+    private void createUser() {
+        // Create User and AppUser for database
+    }
+
+    private boolean validateData() {
+        boolean validUser = true;
+
+        if (this.binding.editAccountNumber.getText().toString().isEmpty()) {
+            this.binding.editAccountNumber.setError("Cannot be empty");
+            validUser = false;
+        }
+        if (this.binding.editInstitutionNumber.getText().toString().isEmpty()) {
+            this.binding.editInstitutionNumber.setError("Cannot be empty");
+            validUser = false;
+        }
+        if (this.binding.editTransitNumber.getText().toString().isEmpty()) {
+            this.binding.editTransitNumber.setError("Cannot be empty");
+            validUser = false;
+        }
+
+        return validUser;
+    }
+} // end of class
