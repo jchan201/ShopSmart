66260e108dfd6683e3e9998869e55a70dc21988f
aronc112 <achin12@myseneca.ca>
2021-10-11 16:20:24 -0400

    Dashboard improvements

---
+++ app/src/main/java/com/shopsmart/shopsmart/CustomerDashboardActivity.java
@@ -0,0 +1,19 @@
+package com.shopsmart.shopsmart;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class CustomerDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_dashboard2);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
