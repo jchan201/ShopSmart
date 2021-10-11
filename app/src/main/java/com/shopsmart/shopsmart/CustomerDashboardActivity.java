659d6f7dea3863dc9affb605676339bab0a26260
aronc112 <achin12@myseneca.ca>
2021-10-11 17:35:37 -0400

    Toolbar etc. updates

--- app/src/main/java/com/shopsmart/shopsmart/CustomerDashboardActivity.java
+++ app/src/main/java/com/shopsmart/shopsmart/CustomerDashboardActivity.java
@@ -11,7 +11,7 @@ public class CustomerDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.customer_dashboard2);
+        setContentView(R.layout.customer_dashboard1);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
